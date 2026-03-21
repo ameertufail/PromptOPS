7736111ecee482c83c5b1f3d02c18268a8ae19f8
Ameer Tufail <ameertufail001@gmail.com>
2026-03-21 09:07:22 +0500

    Create index.ts

---
+++ apps/web/src/lib/eval/index.ts
@@ -0,0 +1,10 @@
+export { EvalEngine } from "./engine";
+export type {
+  EvalEngineCallbacks,
+  EvalEngineConfig,
+  EvalEngineProgress,
+  EvalItemProgress,
+  EvalItemStatus,
+  ItemResultCallback,
+  ProgressCallback
+} from "./types";
